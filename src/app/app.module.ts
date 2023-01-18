ace64bfe7de12547531db7be2d91bce76d89d21b
msdn41 <msdn41@getgroup.com>
2023-01-18 19:51:19 +0200

    push project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, BrowserAnimationsModule,
+    AppRoutingModule, CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Images = [{ id: "1", src: '../assets/images/slider2.jpg' }, { id: "3", src: '../assets/images/slider1.jpg' }, { id: "2", src: '../assets/images/slider3.jpg' }];
+
+  SlideOptions = {
+    items: 1,
+    dots: false,
+    nav: true,
+    navText: ['<i class="fa fa-long-arrow-left"></i>', '<i class="fa fa-long-arrow-right"></i>'],
+    loop: true,
+    autoplay: true
+  };
+
+
+  //sponsors = ['../assets/images/logo1.png', '../assets/images/logo2.png', '../assets/images/logo3.png', '../assets/images/logo4.png', '../assets/images/logo5.png'];
+  sponsors = [{ id: "1", src: '../assets/images/spon1.png' }, { id: "2", src: '../assets/images/spon2.png' }, { id: "3", src: '../assets/images/spon3.png' }, { id: "4", src: '../assets/images/spon4.png' }];
+  CarouselOptions = {
+    items: 2,
+    dots: false,
+    nav: false,
+    autoplay: true,
+    loop: true,
+    responsive: {
+      320: {
+        items: 1
+      },
+      767: {
+        items: 3
+      },
+      991: {
+        items: 4
+      },
+      1024: {
+        items: 5
+      }
+    }
+  };
+
+}
